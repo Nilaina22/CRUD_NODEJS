7943f3d1ff85069ae6dd26e442ad008a842326cf
Batoto <batoto@batoto@batoto.org>
2023-07-23 21:59:43 +0300

    modification du connection au db

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:cZnR7iwFbpYAFqx4@cluster0.yunslut.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://admin:cZnR7iwFbpYAFqx4@cluster0.yunslut.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
