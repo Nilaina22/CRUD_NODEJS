f44a9b8e0110b21b7a3bd30816431af320be4d25
Batoto <batoto@batoto@batoto.org>
2023-07-23 21:12:23 +0300

    first commit

---
+++ models/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin:cZnR7iwFbpYAFqx4@cluster0.yunslut.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') }
+    else { console.log('Error in DB connection : ' + err) }
+});
+
+require('./employee.model');
+
+//'mongodb://localhost:27017/EmployeeDB'
---
+++ server.js
@@ -0,0 +1,31 @@
+require('./models/db');
+
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
+
+const employeeController = require('./controllers/employeeController');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+// Route pour rediriger l'URL de base vers /employee
+app.get('/', (req, res) => {
+    res.redirect('/employee');
+});
+
+// Définir les autres routes à l'aide du employeeController
+app.use('/employee', employeeController);
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.listen(3000, () => {
+    console.log('Express server started at port : 3000');
+});
+
+app.use('/employee', employeeController);
